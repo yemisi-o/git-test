8d50123a83466ef0a965f9f6bb0209e2d1ee7596
oluwayemisi <oluwayemisimolade@gmail.com>
2017-05-18 17:41:10 +0100

    Added iterate route and page to display username in a given no of times.

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ def home():
     return render_template('index.html', username=username)
 
 
-@app.route('/if')
+@app.route('/profile')
 def profile():
     username = request.args.get('username')
     surname = request.args.get('surname')
+    # Condition to check for username and/or surname
     if username and surname:
         username = username + ' ' + surname
     elif username and not surname:
@@ -21,6 +22,21 @@ def profile():
     else:
         username = 'World'
     return render_template('index2.html', username=username)
+
+
+@app.route('/iterate')
+def iterate():
+    username = request.args.get('username')
+    no_of_times = request.args.get('no_of_times')
+
+    # Checks if no_of_times is empty, negative, or zero value, then defaults to 1
+    if not no_of_times or no_of_times.isdigit() is False:
+        no_of_times = 1
+
+    # Split username into names if applicable,capitalize names, and convert list of names to string
+    username = " ".join(names.capitalize() for names in username.split())
+
+    return render_template('index3.html', username=username, no_of_times=int(no_of_times))
 
 
 if __name__ == '__main__':
