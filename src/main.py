681a3a7b134173f96fc17d48b53ab9cd98abba42
oluwayemisi <oluwayemisimolade@gmail.com>
2017-05-20 13:53:04 +0100

    Added and formatted results page

--- src/main.py
+++ src/main.py
@@ -7,5 +7,24 @@ def home():
     username = request.args.get('username')
     return render_template('index.html', username=username)
 
+@app.route('/search')
+def search():
+    data = {
+        "title": "Jinja",
+        "url": "/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=1&amp;cad=rja&amp;uact=8&amp;ved=0ahUKEwi6zevKr_7TAhUJiSwKHblZAeUQFgghMAA&amp;url=http%3A%2F%2Fjinja.pocoo.org%2F&amp;usg=AFQjCNFKiObWsstA_tSpH_tNsUK_VKY4EA&amp;sig2=eFKaff1ZCmdBpLeG3og3Uw",
+        "summary": """ 
+                 <div class="s">
+                    <div>
+                        <div class="f kv _SWb" style="white-space:nowrap"><cite class="_Rm">jinja.pocoo.org/</cite>
+                            <div class="action-menu ab_ctl"><a class="_Fmb ab_button" href="#" id="am-b0" aria-label="Result details" aria-expanded="false"
+                                    aria-haspopup="true" role="button" jsaction="m.tdd;keydown:m.hbke;keypress:m.mskpe" data-ved="0ahUKEwi6zevKr_7TAhUJiSwKHblZAeUQ7B0IIjAA"><span class="mn-dwn-arw"></span></a>
+                            </div>
+                        </div><span class="st"><em>Jinja</em> is Beautiful. {% extends "layout.html" %} {% block body %} &lt;ul&gt; {% for user in users %} &lt;li&gt;&lt;a href="{{ user.url }}"&gt;{{ user.username }}&lt;/a&gt;&lt;/li&gt; {% endfor&nbsp;...</span></div>
+                </div>
+        """
+
+    }
+    return render_template('result.html', results=[data, data], title="Jinja")
+
 if __name__ == '__main__':
     app.run(debug=True)
