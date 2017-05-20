d9522f0387d69f9941ddb31a9406e20e69cc1071
oluwayemisi <oluwayemisimolade@gmail.com>
2017-05-20 11:35:58 +0100

    Built static page for Google clone

--- src/main.py
+++ src/main.py
@@ -7,39 +7,5 @@ def home():
     username = request.args.get('username')
     return render_template('index.html', username=username)
 
-
-@app.route('/profile')
-def profile():
-    username = request.args.get('username')
-    surname = request.args.get('surname')
-    # Condition to check for username and/or surname
-    if username and surname:
-        username = '{} {}'.format(username, surname)
-    elif username and not surname:
-        username = username
-    elif surname and not username:
-        username = surname
-    else:
-        username = 'World'
-    return render_template('index2.html', username=username)
-
-
-@app.route('/iterate')
-def iterate():
-    username = request.args.get('username', default="")
-    surname = request.args.get('surname', default="")
-    no_of_times = request.args.get('no_of_times')
-
-    if not(username or surname):
-        username = 'World'
-
-    username = '{} {}'.format(username.capitalize(), surname.capitalize())
-
-    if not no_of_times or no_of_times.isdigit() is False:
-        no_of_times = 1
-
-    return render_template('index3.html', username=username, no_of_times=int(no_of_times))
-
-
 if __name__ == '__main__':
     app.run(debug=True)
